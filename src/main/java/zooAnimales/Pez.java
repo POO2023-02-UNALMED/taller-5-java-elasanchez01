e9d303a15439577032bd385a802dc6a480a3a329
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-09-26 10:40:34 -0500

    Update Pez.java

--- src/main/java/zooAnimales/Pez.java
+++ src/main/java/zooAnimales/Pez.java
@@ -18,4 +18,50 @@ public class Pez extends Animal{
     this.cantidadAletas = cantidadAletas;
     Pez.listado.add(this);
   }
+
+  public static void setListado(ArrayList<Pez> listado) {
+		Pez.listado=listado;
+	}
+	public static ArrayList<Pez> getListado() {
+		return listado;
+	}
+	
+	public void setColorEscamas(String colorEscamas) {
+		this.colorEscamas=colorEscamas;
+	}
+	public String getColorEscamas() {
+		return colorEscamas;
+	}
+	
+	public void setCantidadAletas(int cantidadAletas) {
+		this.cantidadAletas=cantidadAletas;
+		
+	}
+	public int getCantidadAletas() {
+		return cantidadAletas;
+	}
+	
+	
+	public int cantidadPeces() {
+		return Pez.listado.size();
+		
+	}
+	
+	public String movimiento() {
+		return "nadar";
+	}
+	
+	public static Pez crearSalmon(String nombre, int edad, String genero) {
+		Pez Pez=new Pez(nombre, edad,"oceano", genero, "rojo", 6);
+		salmones++;
+		return Pez;
+		
+	}
+	
+	public static Pez crearBacalao(String nombre, int edad, String genero) {
+		Pez Pez=new Pez(nombre, edad, "oceano", genero, "gris", 6);
+		bacalaos++;
+		return Pez;
+	}
+  
 }
