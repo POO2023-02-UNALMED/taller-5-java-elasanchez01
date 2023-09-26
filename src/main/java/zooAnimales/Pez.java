a582d4f8e6fed437ae17c0a96b106427ba2986eb
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-09-26 03:35:25 -0500

    Update Pez.java

--- src/main/java/zooAnimales/Pez.java
+++ src/main/java/zooAnimales/Pez.java
@@ -1,1 +1,21 @@
+package zooAnimales;
+import java.util.ArrayList;
 
+public class Pez extends Animal{
+  private static ArrayList<Pez> listado = new ArrayList<>();
+  public static int salmones;
+  public static int bacalaos;
+  private String colorEscamas;
+  private int cantidadAletas;
+
+  public Pez(){
+    this(null, 0, null, null, null, 0)
+  }
+
+  public Pez(String nombre, int edad, String habitat, String genero, String colorEscamas, String cantidadAletas){
+    super(nombre, edad, habitat, genero);
+    this.colorEscamas = colorEscamas;
+    this.cantidadAletas = cantidadAletas;
+    Pez.listado.add(this);
+  }
+}
