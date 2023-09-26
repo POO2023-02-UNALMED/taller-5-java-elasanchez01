21d8677367d2fcb47675cf14bdb1a2c2f96c1f9d
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-09-26 02:47:53 -0500

    Update Ave.java

--- src/main/java/zooAnimales/Ave.java
+++ src/main/java/zooAnimales/Ave.java
@@ -1,1 +1,19 @@
+package zooAnimales;
+import java.util.ArrayList;
 
+public class Ave extends Animal{
+  private static ArrayList<Ave> listado = new ArrayList<>();
+  private String colorPlumas;
+  public static int halcones;
+  public statin int aguilas;
+
+  public Ave() {
+    this(null, 0, null, null, null);
+  }
+  
+  public Ave(String nombre, int edad, String habitat, String genero, String colorPlumas){
+    super(nombre, edad, habitat, genero);
+    this.colorPlumas = colorPlumas;
+    Ave.listado.add(this);
+  }
+}
