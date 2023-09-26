3b1ba63caed908764033f7e34a8dd6679184a770
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-09-26 02:34:08 -0500

    Update Anfibio.java

--- src/main/java/zooAnimales/Anfibio.java
+++ src/main/java/zooAnimales/Anfibio.java
@@ -1,1 +1,21 @@
+package zooAnimales;
+import java.util.ArrayList;
 
+public class Anfibio extends Animal {
+  private static ArrayList<Anfibio> listado = new ArrayList<>();
+  private String colorPiel;
+  private boolean venenoso;
+  public static int ranas;
+  public static int salamandras;
+
+  public Anfibio() {
+    this(null, 0, null, null, null, false);
+  }
+  
+  public Anfibio(String nombre, String edad, String habitat, String genero, String colorPiel, boolean venenoso){
+    super(nombre, edad, habitat, genero);
+    this.colorPiel = colorPiel;
+    this.venenoso = venenoso;
+    Anfibio.listado.add(this);
+  }
+}
