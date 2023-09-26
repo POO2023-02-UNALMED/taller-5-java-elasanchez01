cd06166d12d9ad0a25f55518775586c1b67fd9ee
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-09-26 10:41:56 -0500

    Update Reptil.java

--- src/main/java/zooAnimales/Reptil.java
+++ src/main/java/zooAnimales/Reptil.java
@@ -19,4 +19,49 @@ public class Reptil extends Animal{
 		Reptil.listado.add(this);
 		
 	}
+
+	public static void setListado(ArrayList<Reptil> listado) {
+		Reptil.listado=listado;
+	}
+	public static ArrayList<Reptil> getListado() {
+		return listado;
+	}
+	
+	public void setColorEscamas(String colorEscamas) {
+		this.colorEscamas=colorEscamas;
+	}
+	public String getColorEscamas() {
+		return colorEscamas;
+	}
+	
+	public void setLargoCola(int largoCola) {
+		this.largoCola=largoCola;
+	}
+	public int getLargoCola() {
+		return largoCola;
+	}
+	
+	public int cantidadReptiles() {
+		return Reptil.listado.size();
+		
+	}
+	
+	public String movimiento() {
+		return "reptar";
+	}
+	
+	public static Reptil crearIguana(String nombre, int edad, String genero) {
+		Reptil Reptil= new Reptil(nombre, edad, "humedal",genero, "verde",3);
+		iguanas++;
+		return Reptil;
+		
+	}
+	
+	public static Reptil crearSerpiente(String nombre, int edad, String genero) {
+		Reptil Reptil =new Reptil(nombre, edad, "jungla", genero, "blanco", 1);
+		serpientes++;
+		return Reptil;
+		
+	}
+	
 }
