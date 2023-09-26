efef19202722ef9cc7b8b57a3a6d3b9f6bbe4baa
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-09-26 03:57:48 -0500

    Update Reptil.java

--- src/main/java/zooAnimales/Reptil.java
+++ src/main/java/zooAnimales/Reptil.java
@@ -1,1 +1,22 @@
+package zooAnimales;
+import java.util.ArrayList;
 
+public class Reptil extends Animal{
+	private static ArrayList<Reptil> listado=new ArrayList<>();
+	public static int iguanas;
+	public static int serpientes;
+	private String colorEscamas;
+	private int largoCola;
+	
+	public Reptil() {
+		this(null,0,null,null,null,0);
+	}
+	
+	public Reptil(String nombre, int edad, String habitat, String genero, String colorEscamas, int largoCola) {
+		super(nombre,edad,habitat,genero);
+		this.colorEscamas=colorEscamas;
+		this.largoCola=largoCola;
+		Reptil.listado.add(this);
+		
+	}
+}
