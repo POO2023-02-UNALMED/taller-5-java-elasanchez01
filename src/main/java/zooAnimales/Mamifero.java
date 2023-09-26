45d68446a84c79ec53b441404f289b4e7d5659ec
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-09-26 03:25:53 -0500

    Update Mamifero.java

--- src/main/java/zooAnimales/Mamifero.java
+++ src/main/java/zooAnimales/Mamifero.java
@@ -1,1 +1,22 @@
+package zooAnimales;
+import java.util.ArrayList;
 
+public class Mamifero extends Animal{
+  private static ArrayList<Mamifero> listado = new ArrayList<>();
+  public static int caballos = 0;
+  public static int leones = 0;
+  private boolean pelaje;
+  private int patas;
+
+  public Mamifero(){
+    this(null, 0, null, null, false, 0);
+  }
+  
+  public Mamifero(String nombre, int edad, String habitat, String genero, boolean pelaje, int patas) {
+    super(nombre, edad, habitat, genero);
+    this.pelaje = pelaje;
+    this.patas = patas;
+    Mamifero.listado.add(this);
+  }
+  
+}
