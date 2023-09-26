de9f3559f648b1059c8924bcdd307306bcb854c7
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-09-26 02:26:23 -0500

    Update Animal.java

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -1,1 +1,24 @@
+package zooAnimales;
+import gestion.Zona;
 
+public Class Animal {
+  private static int totalAnimales;
+  private String nombre;
+  private int edad;
+  private String genero;
+  private String habitad;
+  private Zona zona;
+
+  public Animal(){
+    this(null, 0, null, null)
+  }
+
+  public Animal(String nombre, int edad, String habitad, String genero){
+    this.nombre = nombre;
+    this.edad = edad;
+    this.habitad = habitad;
+    this.genero = genero;
+    this.zona = null;
+    totalAnimales++;
+  }
+}
