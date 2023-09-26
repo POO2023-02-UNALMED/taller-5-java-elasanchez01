229afbf286e4a435226f3fa9c4ae1710cddb4131
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-09-26 02:06:46 -0500

    Update Zona.java

--- src/main/java/gestion/Zona.java
+++ src/main/java/gestion/Zona.java
@@ -1,1 +1,15 @@
+package gestion;
+import java.util.ArrayList;
+import zooAnimales.Animal;
 
+public class Zona {
+  private String nombre;
+  private Zoologico zoo;
+  private ArrayList<Animal>animales = new ArrayList<>();
+
+  public Zona(String nombre, Zoologico zoo){
+    this.nombre = nombre;
+    this.zoo = zoo
+  }
+
+}
