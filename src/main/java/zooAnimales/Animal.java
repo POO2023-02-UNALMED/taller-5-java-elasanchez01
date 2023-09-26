795120b55cd42fc2cbd110726438d34b25e48c93
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-09-26 10:35:51 -0500

    Update Animal.java

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -29,6 +29,47 @@ public class Animal {
   public static void getTotalAnimales(){
     return totalAnimales;
   }
+
+  public void setNombre(String nombre) {
+		this.nombre=nombre;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setEdad(int edad) {
+		this.edad=edad;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	
+	public void setHabitat(String habitat) {
+		this.habitat=habitat;
+	}
+	public String getHabitat() {
+		return habitat;
+	}
+	
+	public void setGenero(String genero) {
+		this.genero=genero;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	
+	public void setZona(Zona zona) {
+		this.zona=zona;
+	}
+	public Zona getZona() {
+		return zona;
+	}
+	
+	
+	public String movimiento() {
+		return "desplazarse";
+	}
+	
   public static String totalPorTipo() {
     return "Mamiferos: "+Mamifero.getListado().size()+"\n"+
       "Aves: "+Ave.getListado().size()+"\n"+
@@ -36,4 +77,12 @@ public class Animal {
       "Peces: "+Pez.getListado().size()+"\n"+
       "Anfibios: "+Anfibio.getListado().size();
   }
+  public String toString() {
+		if (getZona()!=null) {
+			return "Mi nombre es "+ getNombre()+", tengo una edad de " +getEdad()+ ", habito en "+getHabitat()+" y mi genero es "+getGenero()+", la zona en la que me ubico es "+getZona().getNombre()+", en el "+getZona().getZoo().getNombre();
+		}
+		else {
+		    return "Mi nombre es "+ getNombre()+", tengo una edad de " +getEdad()+ ", habito en "+getHabitat()+" y mi genero es "+getGenero();
+		}
+	}
 }
