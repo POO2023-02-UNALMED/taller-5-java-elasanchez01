b1f7f11573a6ac63bb4c18be916cd2af14418f70
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-09-26 02:11:09 -0500

    Update Zoologico.java

--- src/main/java/gestion/Zoologico.java
+++ src/main/java/gestion/Zoologico.java
@@ -1,1 +1,17 @@
+package gestion;
+import java.util.ArrayList;
 
+public class Zoologico {
+  private String nombre;
+  private String ubicacion;
+  private ArrayList<Zona> zonas = new ArrayList<>();
+
+  public Zoologico() {
+    this(null, null);
+  }
+
+  public Zoologico(String nombre, String ubicacion){
+    this.nombre = nombre;
+    this.ubicacion = ubicacion;
+  }
+}
